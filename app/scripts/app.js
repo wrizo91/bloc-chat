595801d131d03006b180a826afd975a4aac6a024
warizo16 <wrizo91@gmail.com>
2017-12-16 10:44:54 -0600

    Finished basic setup

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+(function() {
+    function config($locationProvider, $stateProvider) {
+         $locationProvider
+             .html5Mode({
+                 enabled: true,
+                 requireBase: false
+              });
+ 
+         $stateProvider
+             .state('home', {
+                 url: '/',
+                 controller: 'HomeCtrl as home',
+                 templateUrl: '/templates/home.html'
+             });
+     }
+         
+     angular
+         .module('bloc-chat', ['ui.router', 'firebase'])
+         .config(config);
+ })();
