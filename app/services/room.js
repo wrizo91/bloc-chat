06cd2b6ce94f959edbeb7a19f91585d890472218
warizo16 <wrizo91@gmail.com>
2017-12-21 12:49:12 -0600

    checkpoint 2 correction

--- app/services/room.js
+++ app/services/room.js
@@ -1,12 +1,13 @@
 (function() {
   function Room($firebaseArray) {
-    var Room = {};
+    
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
 
-    Room.all = rooms;
-
-    return Room;
+    return{
+    
+        all: rooms
+    }; 
   }
 
   angular
