2dbf693f42700bfe99658a40db764bf21548723a
warizo16 <wrizo91@gmail.com>
2017-12-28 17:45:22 -0600

    checkpoint 6 correction

--- app/services/Message.js
+++ app/services/Message.js
@@ -1,24 +1,29 @@
+
 (function() {
-    function Message($firebaseArray) {
-        var Message = {};
-        
-        var ref = firebase.database().ref().child("messages");
-        var messages = $firebaseArray(ref);
-        Message.all = messages;
-        
-        Message.getByRoomId = function (roomId) {
-            return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
-        }
-        
-        Message.send = function (newMessage) {
-            messages.$add(newMessage);
-            newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
-        }
-        
-        return Message;
+  function Message($firebaseArray) {
+    var Message = {};
+      
+    var messageRef = firebase.database().ref().child("messages");
+    var messages = $firebaseArray(messageRef);
+    
+    Message.getByRoomId = function(roomId) {
+        return $firebaseArray(messageRef.orderByChild('roomId').equalTo(roomId));
     }
     
-    angular
-        .module('blocChat')
-        .factory('Message', ['$firebaseArray', Message]);
+    Message.send = function(newMessage){
+        messages.$add({
+            username: newMessage.username,
+            content: newMessage.content,
+            sentAt: Date.now(),
+            roomId: newMessage.roomId
+        });
+    }
+      
+    return Message; 
+    
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
 })();
