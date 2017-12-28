46c5a75d5c3bc7e02a2cade4f108febfd121b49b
warizo16 <wrizo91@gmail.com>
2017-12-28 17:38:18 -0600

    checkpoint 5 correction

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,16 +1,26 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
-        this.rooms = Room.all;
-        this.addRoom = function() {
+    function HomeCtrl(Room, Message, $uibModal, $cookies) {
+        var home = this;
+        home.rooms = Room.all;
+        home.currentRoom = null;
+        home.currentUser = $cookies.get('blocChatCurrentUser');
+        
+        home.addRoom = function() {
             $uibModal.open({
                 templateUrl: '/templates/modal.html',
                 size: 'sm',
                 controller: 'ModalCtrl as modal'
             });
         }
+        
+        home.setCurrentRoom = function (room) {
+            home.currentRoom = room;
+            home.messages = Message.getByRoomId(home.currentRoom.$id);
+        }
+        
     }
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', '$cookies', HomeCtrl]);
 })();
